5d253adfec9c3cc14bbcc0e80af8f4520d3980c0
Tyler <wh7211@gmail.com>
2019-04-07 00:14:32 +0800

    a

--- python从入门到实践/alien_invasion/1 最终代码/alien_invasion.py
+++ python从入门到实践/alien_invasion/1 最终代码/alien_invasion.py
@@ -11,6 +11,7 @@
 import pygame
 from pygame.sprite import Group
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 import game_functions as gf
 
@@ -24,6 +25,9 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
 
     pygame.display.set_caption("Alien Invasion")
+
+    # 创建一个用于存储游戏统计信息的实例
+    stats = GameStats(ai_settings)
 
     # 创建一艘飞船、一个用于存储子弹的编组和一个用于存储外星人的编组
     ship = Ship(ai_settings, screen)
@@ -40,7 +44,7 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-        gf.update_aliens(ai_settings, aliens)
+        gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
